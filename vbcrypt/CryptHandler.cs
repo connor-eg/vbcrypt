e0c5d92dc17fc41a0db1d12c44ff9d59c0b991f5
connor-eg <connoregarcia@gmail.com>
2023-10-17 10:40:37 -0500

    Massive cleanup

---
+++ vbcrypt/CryptHandler.cs
@@ -0,0 +1,104 @@
+﻿using System.Security.Cryptography;
+
+internal class CryptHandler : IDisposable
+{
+    private SymmetricAlgorithm CryptAlgorithmInstance;
+    private HashAlgorithm HashAlgorithmInstance;
+
+    public CryptHandler(SymmetricAlgorithm CryptAlgorithmInstance, HashAlgorithm HashAlgorithmInstance, int keySize = 256)
+    {
+        this.CryptAlgorithmInstance = CryptAlgorithmInstance;
+        this.CryptAlgorithmInstance.KeySize = keySize;
+
+        this.HashAlgorithmInstance = HashAlgorithmInstance;
+    }
+
+    // F a n c y . Allows a "using" block to manage instances of this class.
+    public void Dispose()
+    {
+        CryptAlgorithmInstance.Clear();
+    }
+
+    public void HashAndSetKey(byte[] bytes)
+    {
+        CryptAlgorithmInstance.Key = HashAlgorithmInstance.ComputeHash(bytes);
+    }
+
+    public void Decrypt(string[] files, bool deleteOnFinish)
+    {
+        foreach (string file in files)
+        {
+            if (!File.Exists(file))
+            {
+                Console.WriteLine($"Cannot find/access file {file}");
+                continue;
+            }
+
+            Console.Write($"Processing {file}... ");
+
+            bool terseName = file.EndsWith(".vbcrypt");
+            string outFileName = terseName ? $"{file[..^8]}" : $"{file}.decrypted";
+            try
+            {
+                using (FileStream inStream = File.OpenRead(file))
+                {
+                    byte[] recoveredIV = new byte[16];
+                    if (inStream.Read(recoveredIV, 0, 16) < 16) throw new EndOfStreamException("failed. File is too small to contain any encrypted data.");
+                    CryptAlgorithmInstance.IV = recoveredIV;
+                    using FileStream outStream = File.OpenWrite(outFileName);
+                    using CryptoStream cStream = new(inStream, CryptAlgorithmInstance.CreateDecryptor(), CryptoStreamMode.Read);
+                    cStream.CopyTo(outStream);
+                    outStream.Flush();
+                    cStream.Clear();
+                }
+                Console.WriteLine(terseName ? "done." : "done. Remember to remove '.decrypted' from the resulting file.");
+                if (deleteOnFinish) File.Delete(file);
+            }
+            catch (Exception e)
+            {
+                Console.Write("failed. Reason: ");
+                Console.WriteLine(e.Message);
+                File.Delete(outFileName);
+            }
+        }
+    }
+
+    public void Encrypt(string[] files, bool deleteOnFinish)
+    {
+        foreach (string file in files)
+        {
+            if (!File.Exists(file))
+            {
+                Console.WriteLine($"Cannot find file {file}");
+                continue;
+            }
+
+            Console.Write($"Processing {file}... ");
+
+            CryptAlgorithmInstance.GenerateIV();
+
+            try
+            {
+                using (FileStream inStream = File.OpenRead(file))
+                {
+                    using FileStream outStream = File.OpenWrite($"{file}.vbcrypt");
+                    using CryptoStream cStream = new(outStream, CryptAlgorithmInstance.CreateEncryptor(), CryptoStreamMode.Write);
+                    outStream.Write(CryptAlgorithmInstance.IV, 0, 16);
+                    inStream.CopyTo(cStream);
+                    cStream.FlushFinalBlock();
+                    cStream.Clear();
+                }
+                Console.WriteLine("done.");
+                if (deleteOnFinish) File.Delete(file);
+
+            }
+            catch (Exception e)
+            {
+                Console.Write("failed. Reason: ");
+                Console.WriteLine(e.Message);
+                File.Delete($"{file}.vbcrypt");
+            }
+        }
+    }
+
+}
--- vbcrypt/Program.cs
+++ vbcrypt/Program.cs
@@ -4,8 +4,6 @@ using System.Text;
 
 internal class Program
 {
-    private static SHA256 sha = SHA256.Create();
-    private static Aes aes = Aes.Create();
     private static void Main(string[] args)
     {
         Dictionary<string, Argument> parsedArgs;
@@ -35,96 +33,20 @@ internal class Program
         string runMode = parsedArgs["mode"].GetValue();
         string[] fileNames = parsedArgs["files"].GetValues();
 
-        // Generate an AES key (256 bits) from the password.
-        // The hash isn't for security, it just normalizes the input key to a fixed size
-        aes.KeySize = 256;
-        aes.Key = sha.ComputeHash(Encoding.ASCII.GetBytes(phrase));
+        // This looks so much cleaner than before :)
+        // Just look at that using block go!
+        using (CryptHandler cryptHandler = new(Aes.Create(), SHA256.Create(), 256))
+        {
+            // This makes assumptions (bad), but it's the best I know how to do at this moment.
+            // It will work for ASCII strings and UTF8 though, so that should be good enough.
+            cryptHandler.HashAndSetKey(Encoding.UTF8.GetBytes(phrase));
 
-        // Now it's time to do the thing with the stuff.
-        if (runMode == "e") Encrypt(fileNames, false);
-        if (runMode == "ex") Encrypt(fileNames, true);
-        if (runMode == "d") Decrypt(fileNames, false);
-        if (runMode == "dx") Decrypt(fileNames, true);
-
-        aes.Clear();
+            if (runMode == "e") cryptHandler.Encrypt(fileNames, false);
+            if (runMode == "ex") cryptHandler.Encrypt(fileNames, true);
+            if (runMode == "d") cryptHandler.Decrypt(fileNames, false);
+            if (runMode == "dx") cryptHandler.Decrypt(fileNames, true);
+        }
 
         Console.WriteLine("Done.");
-    }
-    
-    private static void Encrypt(string[] files, bool deleteOnFinish)
-    {
-        foreach(string file in files)
-        {
-            if (!File.Exists(file))
-            {
-                Console.WriteLine($"Cannot find file {file}");
-                continue;
-            }
-
-            Console.Write($"Processing {file}... ");
-
-            aes.GenerateIV();
-
-            try
-            {
-                using (FileStream inStream = File.OpenRead(file))
-                {
-                    using FileStream outStream = File.OpenWrite($"{file}.vbcrypt");
-                    using CryptoStream cStream = new(outStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
-                    outStream.Write(aes.IV, 0, 16);
-                    inStream.CopyTo(cStream);
-                    cStream.FlushFinalBlock();
-                    cStream.Clear();
-                }
-                Console.WriteLine("done.");
-                if (deleteOnFinish) File.Delete(file);
-                
-            }
-            catch (Exception e)
-            {
-                Console.Write("failed. Reason: ");
-                Console.WriteLine(e.Message);
-                File.Delete($"{file}.vbcrypt");
-            }
-        }
-    }
-
-    private static void Decrypt(string[] files, bool deleteOnFinish)
-    {
-        foreach (string file in files)
-        {
-            if (!File.Exists(file))
-            {
-                Console.WriteLine($"Cannot find/access file {file}");
-                continue;
-            }
-
-            Console.Write($"Processing {file}... ");
-
-            bool terseName = file.EndsWith(".vbcrypt");
-            string outFileName = terseName ? $"{file[..^8]}" : $"{file}.decrypted";
-            try
-            {
-                using (FileStream inStream = File.OpenRead(file))
-                {
-                    byte[] recoveredIV = new byte[16];
-                    if (inStream.Read(recoveredIV, 0, 16) < 16) throw new EndOfStreamException("failed. File is too small to contain any encrypted data.");
-                    aes.IV = recoveredIV;
-                    using FileStream outStream = File.OpenWrite(outFileName);
-                    using CryptoStream cStream = new(inStream, aes.CreateDecryptor(), CryptoStreamMode.Read);
-                    cStream.CopyTo(outStream);
-                    outStream.Flush();
-                    cStream.Clear();
-                }
-                Console.WriteLine(terseName ? "done." : "done. Remember to remove '.decrypted' from the resulting file.");
-                if (deleteOnFinish) File.Delete(file);
-            }
-            catch (Exception e)
-            {
-                Console.Write("failed. Reason: ");
-                Console.WriteLine(e.Message);
-                File.Delete(outFileName);
-            }
-        }
     }
 }
